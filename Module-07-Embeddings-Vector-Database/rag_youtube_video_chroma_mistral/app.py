e649cee0c740c855abed7dd0d9446b028a1cfcd1
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2024-07-03 16:59:38 +0530

    Update app.py

--- Module-07-Embeddings-Vector-Database/rag_youtube_video_chroma_mistral/app.py
+++ Module-07-Embeddings-Vector-Database/rag_youtube_video_chroma_mistral/app.py
@@ -1,4 +1,5 @@
 #Running:)
+# Enter video link from UI that must have captions. example_video = "https://www.youtube.com/watch?v=4O1rs7mrNDo"
 import streamlit as st
 from mistralai.client import MistralClient
 from langchain_community.vectorstores import Chroma
