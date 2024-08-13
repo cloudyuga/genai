aeba4a521543d1ac43c862b17efaa71b66fb7c71
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2024-08-13 12:20:02 +0530

    Update app.py

--- Module-10-Introduction-to-LlamaIndex/langchain-llamaindex-demo/app.py
+++ Module-10-Introduction-to-LlamaIndex/langchain-llamaindex-demo/app.py
@@ -1,4 +1,7 @@
 # In this app we will use data directory to get the contents. 
+# By default LlamaIndex uses text-embedding-ada-002, which is the default embedding used by OpenAI. please refer below link.
+# https://docs.llamaindex.ai/en/stable/understanding/indexing/indexing/
+
 import os
 import streamlit as st
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
@@ -22,8 +25,8 @@ else:
     if not documents:
         st.error("No documents found in the 'data' directory.")
     else:
-        # Create an index from documents
-        index = VectorStoreIndex.from_documents(documents, llm=llm)
+        # Create an index from documents with default embedding
+        index = VectorStoreIndex.from_documents(documents, llm=llm)  
 
         # Streamlit app interface
         st.title("LlamaIndex-LangChain Demo")
