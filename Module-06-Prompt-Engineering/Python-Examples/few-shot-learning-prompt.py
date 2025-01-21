6326e6bbc08101e1701b04110ccf4c5b9c3ad84a
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2025-01-21 16:47:19 +0530

    Create few-shot-learning-prompt.py

---
+++ Module-06-Prompt-Engineering/Python-Examples/few-shot-learning-prompt.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+ 
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=api_key)
+        
+
+prompt = """Translate the following sentence:
+- "Hello, how are you?" -> "Bonjour, comment ça va?"
+- "What is your name?" -> "Comment tu t'appelles?"
+- "I love programming." ->"""
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": prompt}
+    ],
+    max_tokens=50
+)
+print(response.choices[0].message.content)
