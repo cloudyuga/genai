328b1bf896a84a8fe4492695604e740a20362da1
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2025-01-23 14:31:32 +0530

    Create area-of-rectangle.py

---
+++ Module-00-Basic-Python/2-Functions/area-of-rectangle.py
@@ -0,0 +1,9 @@
+def calculate_area(width, height):
+    area = width * height
+    return area
+
+area1 = calculate_area(5, 10)
+print("Area of rectangle 1:", area1)
+
+area2 = calculate_area(3, 4)
+print("Area of rectangle 2:", area2)
