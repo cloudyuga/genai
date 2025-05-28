a36d136ffb4418dbcbaad06cfe0dbdddfe9866a8
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2025-05-29 01:17:05 +0530

    Update db_utils.py

--- Module-18-Final-Project-Full-Stack-GenAI-Application/Recruitment_Automation_System/db_utils.py
+++ Module-18-Final-Project-Full-Stack-GenAI-Application/Recruitment_Automation_System/db_utils.py
@@ -1,14 +1,17 @@
-# db_utils.py
+import os
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+import chromadb
 
 embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-# Create/load Chroma collection
-vectordb = Chroma(
-    collection_name="jd_collection",
-    persist_directory="chroma_store",
-    embedding_function=embedding_function
+chroma_client = chromadb.HttpClient(
+    host="chroma",  # Replace with your service name
+    port=8000
 )
 
-jd_collection = vectordb
+jd_collection = Chroma(
+    client=chroma_client,
+    collection_name="jd_collection",
+    embedding_function=embedding_function
+)
