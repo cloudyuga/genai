26c81b39f30df4df222d43307d09cbc80fa0454c
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2025-06-05 16:19:47 +0530

    Update persist_db_utils.py

--- Module-18-Final-Project-Full-Stack-GenAI-Application/Recruitment_Automation_System/persist_db_utils.py
+++ Module-18-Final-Project-Full-Stack-GenAI-Application/Recruitment_Automation_System/persist_db_utils.py
@@ -1,1 +1,13 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 
+embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Create/load Chroma collection
+vectordb = Chroma(
+    collection_name="jd_collection",
+    persist_directory="chroma_store",
+    embedding_function=embedding_function
+)
+
+jd_collection = vectordb
